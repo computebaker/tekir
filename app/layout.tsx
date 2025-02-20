ae1055599b7a5d4c2048f46e19ce9074ba6739c1
islemci <musti@duck.com>
2025-02-20 20:43:13 +0300

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div id="global-tag">
+          <script defer src="https://telemetry.tekir.co/script.js" data-website-id="71b0a4f4-071d-4e5f-a39f-203dbfa92d5c"></script>
+          </div>
           {children}
         </ThemeProvider>
       </body>
