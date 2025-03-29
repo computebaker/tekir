f9b077e9f245e5433e6abce3e6c4bd5b445dc6cd
musti <musti@duck.com>
2025-03-29 22:45:10 +0300

    fixes

--- next.config.js
+++ next.config.js
@@ -4,6 +4,20 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  transpilePackages: ['undici'],
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.m?js$/,
+      include: /node_modules[\\/]undici/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['next/babel'],
+        },
+      },
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
