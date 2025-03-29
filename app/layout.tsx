51aa785a99d7afa5019cf292ce1e141db8c3f655
musti <musti@duck.com>
2025-03-29 22:48:50 +0300

    remove tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,6 @@ export default function RootLayout({
         <meta name="description" content="Tekir is a fast, open-source, and privacy-focused search engine." />
         <link rel="icon" href="/favicon.ico" />
         <link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="Tekir" />
-
-        <script defer src="https://telemetry.tekir.co/script.js" data-website-id="71b0a4f4-071d-4e5f-a39f-203dbfa92d5c"></script>
       </head>
       <body>
         <ThemeProvider
