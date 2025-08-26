d5fcc2d90876ec49e459b3024f564a9f6d48039e
musti <musti@duck.com>
2025-08-26 11:05:34 +0300

    fixed robots

--- app/robots.ts
+++ app/robots.ts
@@ -3,11 +3,9 @@ import type { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
-      // Allow only the main page (i.e. the exact root URL):
       {
         userAgent: '*',
-        allow: '/$',
-        disallow: '/',
+        allow: '/',
       }
     ]
   }
